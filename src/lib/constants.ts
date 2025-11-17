997fb231fd498b636df9d8a3913bb558a66c9550
vibesengineering <205912982+vibesengineering@users.noreply.github.com>
2025-11-17 19:15:32 +0000

    Code generation: 595d5a2f

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,5 @@
 export const PROJECT_TITLE = "FlashBoobs";
-export const PROJECT_DESCRIPTION = "by @willywarrior";
+export const PROJECT_DESCRIPTION = "Flash your assets on Farcaster";
 export const PROJECT_CREATOR = "willywarrior";
 export const PROJECT_AVATAR_URL =
   "https://wrpcd.net/cdn-cgi/imagedelivery/BXluQx4ige9GuW0Ia56BHw/37460dae-996e-481a-9c2c-0374da719400/anim=false,fit=contain,f=auto,w=576";
